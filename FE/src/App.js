faa50f932746cdcf4fbc560dee9b1ec748606e64
zger0 <127360974+zger0@users.noreply.github.com>
2023-10-04 17:02:52 +0900

    테일윈드추가

--- FE/src/App.js
+++ FE/src/App.js
@@ -4,6 +4,7 @@ import router from './routes.jsx';
 function App() {
   return (
     <div className="App">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <RouterProvider router={router} />
     </div>
   );
